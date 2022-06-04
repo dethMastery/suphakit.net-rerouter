32f2354ed806e4e5ea3b92aaa685ef4164f88774
dethMastery <owner@detzz.in.th>
2022-06-04 11:19:52 +0700

    updated: router :D

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 let exp = require('express')
 let path = require('path')
 let app = exp()
-let wPort = 8090
+let wPort = 4000
+
+app.use(exp.static('./static'))
+app.use(exp.static(__dirname))
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'jade')
@@ -10,7 +13,25 @@ function homePage(req, res) {
     res.render('index.jade')
 }
 
-app.get('/', homePage)
+app.get('/', function (req, res) {
+    res.sendFile(path.resolve(__dirname, './static/index.html'))
+})
+
+app.get('/donation', function (req, res) {
+    res.sendFile(path.resolve(__dirname, './static/donation.html'))
+})
+
+app.get('/about', function (req, res) {
+    res.sendFile(path.resolve(__dirname, './static/about.html'))
+})
+
+app.get('/payment', function (req, res) {
+    res.sendFile(path.resolve(__dirname, './static/payment.html'))
+})
+
+app.get('/logo.png', function (req, res) {
+    res.sendFile(path.resolve(__dirname, './static/logo.png'))
+})
 
 app.listen(wPort, () => {
     console.log('Express starting @ http://localhost:' + wPort);
